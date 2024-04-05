ad78e1905dacce33dc80fb04883d61997dd458d0
syx <personal_sunyunxian@foxmail.com>
2024-04-05 14:57:58 +0800

    02-exercises

---
+++ book/C-Programming-A-Modern-Approach-2nd-Edition/02/exercises/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    float c, d;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("a = %f, b = %d\n", c, d);
+
+    return 0;
+}
---
+++ book/C-Programming-A-Modern-Approach-2nd-Edition/compiler.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# 编译单个 C 文件脚本
+# 编译成同名二进制文件，并默认放到 build 文件夹下
+# 执行编译后的二进制文件
+
+# check input
+[[ "${1}" ]] && cfile="${1}" || exit 1
+
+all_cfiles="$*"
+
+build_fn="build"
+cfile_out=$(echo "${cfile%.*}" | awk -F "/" '{print $NF}')
+[[ -d "${build_fn}" ]] || mkdir "${build_fn}"
+
+printf "compile %s to %s/%s\n" "${cfile}" "${build_fn}" "${cfile_out}"
+compiler_cli="gcc ${all_cfiles} -o ${build_fn}/${cfile_out} -Wall -w -pedantic -std=c99"
+printf "compil cli: %s\n\n" "${compiler_cli}"
+
+${compiler_cli}
+
+printf "run %s/%s\n" "${build_fn}" "${cfile_out}"
+
+"./${build_fn}/${cfile_out}"
